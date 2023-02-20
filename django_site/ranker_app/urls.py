06a3c41c1feaa2abd2f6d389da890f31c8b17432
TheNathanSpace <wasabimath@gmail.com>
2023-02-20 11:03:31 -0700

    Changed gitignore

--- django_site/ranker_app/urls.py
+++ django_site/ranker_app/urls.py
@@ -3,5 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    # ex: /ranker/
+    path("", views.index, name = "index"),
+
+    # ex: /ranker/[playlist_id]/
+    path("<str:playlist_id>/", views.playlist, name = "detail"),
+
+    # ex: /ranker/[playlist_id]]/rank/
+    path("<str:playlist_id>/rank/", views.playlist_rank, name = "results"),
+
+    # ex: /[user_id]/
+    path("<str:user_id>/", views.user, name = "user"),
 ]
--- django_site/ranker_app/views.py
+++ django_site/ranker_app/views.py
@@ -2,4 +2,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("You're at the song ranking index.")
+
+
+def playlist(request, playlist_id):
+    return HttpResponse(f"You're looking at playlist {playlist_id}.")
+
+
+def playlist_rank(request, playlist_id):
+    return HttpResponse(f"You're ranking songs in playlist {playlist_id}.")
+
+
+def user(request, user_id):
+    return HttpResponse(f"You're viewing user {user_id}'s playlists.")
