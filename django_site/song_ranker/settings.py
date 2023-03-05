e50afb424029c8c89c7308dfdb4f991c070ab184
TheNathanSpace <wasabimath@gmail.com>
2023-03-05 16:22:04 -0700

    final commit

--- django_site/song_ranker/settings.py
+++ django_site/song_ranker/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-import my_secrets
+import django_site.song_ranker.my_secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
